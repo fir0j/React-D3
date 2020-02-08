4d4bb40942f5f3f48e4d0ed098d37dadcc3c6975
fir0j <firoj.is.availble@gmail.com>
2020-02-08 15:42:55 +0530

    Updates

---
+++ precache-manifest.3445fe2b8d06f28aeda1077dd81c13b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f118b3121d9dc5093c215f83334e6b9",
+    "url": "/React-D3/index.html"
+  },
+  {
+    "revision": "5f7c11f84117493419b3",
+    "url": "/React-D3/static/css/main.1cbcd143.chunk.css"
+  },
+  {
+    "revision": "b3f88236aa127dce9a86",
+    "url": "/React-D3/static/js/2.0b8127cc.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/React-D3/static/js/2.0b8127cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f7c11f84117493419b3",
+    "url": "/React-D3/static/js/main.e8899122.chunk.js"
+  },
+  {
+    "revision": "8e7b42d56c6b2e172350",
+    "url": "/React-D3/static/js/runtime-main.389640b5.js"
+  }
+]);
