07ee91acddc0160a34bc838f22c15d77a48d6979
fir0j <firoj.is.availble@gmail.com>
2020-02-10 09:59:08 +0530

    added radialChart

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { DomControl } from './components/DomControl.component';
 import * as d3 from 'd3';
 
-const width = 650;
+const width = 600;
 const height = 400;
 // const margin = { top: 20, right: 5, bottom: 20, left: 35 };
 
@@ -68,6 +68,31 @@ const App = () => {
 		];
 	};
 
+	const radialChartData = () => {
+		const radiusScale = d3.scaleLinear().range([ 0, width / 2 ]);
+		const colorScale = d3.scaleSequential(d3.interpolateSpectral);
+
+		const tempMax = d3.max(data, (d) => d.high);
+		const [ minAvg, maxAvg ] = d3.extent(data, (d) => d.avg);
+		radiusScale.domain([ 0, tempMax ]);
+		colorScale.domain([ maxAvg, minAvg ]);
+
+		// one arc per day, innerRadius is low temp, outerRadius is high temp
+		const arcGenerator = d3.arc();
+		const perSliceAngle = 2 * Math.PI / data.length;
+		return data.map((d, i) => {
+			return {
+				fill: colorScale(d.avg),
+				path: arcGenerator({
+					startAngle: i * perSliceAngle,
+					endAngle: (i + 1) * perSliceAngle,
+					innerRadius: radiusScale(d.low),
+					outerRadius: radiusScale(d.high)
+				})
+			};
+		});
+	};
+
 	return (
 		<div className="flex flex-wrap justify-around">
 			<div className="border mt-2">
@@ -95,8 +120,12 @@ const App = () => {
 				{data && (
 					<div>
 						<svg width={width} height={height}>
-							{barChartData().map((item) => (
-								<rect x={item.x} y={item.y} width="2" height={item.height} fill={item.fill} />
+							{radialChartData().map((item) => (
+								<path
+									d={item.path}
+									fill={item.fill}
+									transform={`translate(${width / 2}, ${height / 2})`}
+								/>
 							))}
 						</svg>
 					</div>
